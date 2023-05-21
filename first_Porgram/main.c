f8f3eafc334823380cd28c096e4994e451ebd54d
Sasha Johnson <sashkajohnson@gmail.com>
2023-05-20 23:07:46 -0500

    First Test File

---
+++ first_Porgram/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Please enter a Number: \n");
+
+    int number;
+
+    scanf("%d", &number);
+
+    printf("Your number is %d", number);
+    return 0;
+}
